08a73d8e92e5809b1c189ef6945211273dc1850a
nttaif <nt.tai5304@gmail.com>
2024-09-29 12:03:02 +0700

    delete node module

--- Backend/server2/tortoise-nest-online/dist/module/courses/dto/update-course.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateCourseDto } from './create-course.dto';
-declare const UpdateCourseDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateCourseDto>>;
-export declare class UpdateCourseDto extends UpdateCourseDto_base {
-}
-export {};
