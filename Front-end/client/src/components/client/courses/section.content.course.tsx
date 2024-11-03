33f258c58261812c0ff35703d2ed8c5b8e498b54
nhan080804 <nhan080804@gmail.com>
2024-11-03 11:53:42 +0700

    fix bug fetch data for get list coureses [front-end]

--- Front-end/client/src/components/client/courses/section.content.course.tsx
+++ Front-end/client/src/components/client/courses/section.content.course.tsx
@@ -21,7 +21,7 @@ export default function SectionContentCourse(props: IProps) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const [isShowList, setShowList] = useState(false);
-  const [currentPage, setCurrentPage] = useState(meta.current);
+  const [currentPage, setCurrentPage] = useState(meta?.current);
   const [filteredCourses, setFilteredCourses] = useState(courses);
   const [selectedCategories, setSelectedCategories] = useState<string[]>([
     "all",
@@ -328,7 +328,7 @@ export default function SectionContentCourse(props: IProps) {
               <path d="M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z"></path>
             </svg>
           </button>
-          {[...Array(meta.pages)].map((_, index) => (
+          {[...Array(meta?.pages)].map((_, index) => (
             <button
               key={index}
               className={`font-bold py-3 px-5 rounded-full ${currentPage === index + 1 ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-600"}`}
@@ -337,12 +337,12 @@ export default function SectionContentCourse(props: IProps) {
               {index + 1}
             </button>
           ))}
-          <button onClick={() => onChange(currentPage + 1)} disabled={currentPage === meta.pages}>
+          <button onClick={() => onChange(currentPage + 1)} disabled={currentPage === meta?.pages}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 mt-4">
               <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
             </svg>
           </button>
-          <div>Tổng trang {meta.pages}</div>
+          <div>Tổng trang {meta?.pages}</div>
         </div>
       </div>
     </>
